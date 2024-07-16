0350fb83b6ec6e8ee839954b28fb2ec431984d98
Abdelrhman Haroun <106913370+Abdelrhman-Haroun@users.noreply.github.com>
2024-07-17 02:45:41 +0300

    first-commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,130 @@
+﻿using E_Commerce.DTO;
+using E_Commerce.IRepository;
+using E_Commerce.Models;
+using E_Commerce.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        UserManager<ApplicationUser> userManager;
+        SignInManager<ApplicationUser> signInManager;
+        IUser repository;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IUser repository)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.repository = repository;
+        }
+        [HttpPost]
+        [Route("RegisterUser")]
+        public async Task<IActionResult> Regesteration(ApplicationUserDTO applicationUser)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser()
+                {
+                    UserName = applicationUser.Email,
+                    FirstName = applicationUser.FirstName,
+                    LastName = applicationUser.LastName,
+                    Email = applicationUser.Email,
+                    PasswordHash = applicationUser.Password
+                };
+                var result = await userManager.CreateAsync(user, applicationUser.Password);
+                if (result.Succeeded)
+                {
+                   //await signInManager.SignInAsync(user, false);
+                    return new OkObjectResult("User Register Successfully ");
+                }
+                return new BadRequestObjectResult("There was an error processing your request, please try again."); ;
+            }
+            return BadRequest();
+        }
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(UserLoginDTO userDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await userManager.FindByEmailAsync(userDTO.Email);
+                if (result != null)
+                {
+                    bool check =await userManager.CheckPasswordAsync(result, userDTO.Password);
+                    if (check)
+                    {
+                        await signInManager.SignInAsync(result, userDTO.RemeberMe);
+                        return new OkObjectResult("User LoggedIn Successfully");
+                    }
+                    return new BadRequestObjectResult("Invalid Password");
+                }
+                return new BadRequestObjectResult("Invalid Email");
+            }
+            return BadRequest(userDTO);
+        }
+        [HttpPost]
+        [Route("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return new OkObjectResult("User LoggedOut Successfully");
+        }
+        [HttpGet]
+        [Route("Get Account Details")]
+        public async Task<IActionResult> Get()
+        {
+            var result = await userManager.GetUserAsync(User);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return new BadRequestObjectResult("Please Login First");
+        }
+        [HttpPost]
+        [Route("UpdateUser")]
+        public async Task<IActionResult> UpdateUser(ApplicationUserDTO updatedUser)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingUser = await userManager.FindByEmailAsync(updatedUser.Email);
+                if (existingUser != null)
+                {
+                    // Update user properties
+                    existingUser.FirstName = updatedUser.FirstName;
+                    existingUser.LastName = updatedUser.LastName;
+
+                    // Update user in the database
+                    var result = await userManager.UpdateAsync(existingUser);
+                    if (result.Succeeded)
+                    {
+                        return new OkObjectResult("User information updated successfully");
+                    }
+                    else
+                    {
+                        return new BadRequestObjectResult("Failed to update user information");
+                    }
+                }
+                else
+                {
+                    return new BadRequestObjectResult("User not found");
+                }
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+        [HttpGet]
+        [Route("Get All Users")]
+        public IActionResult GetAll()
+        {
+            var users = repository.GetAll();
+            return Ok(users);
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,118 @@
+﻿using E_Commerce.DTO;
+using E_Commerce.IRepository;
+using E_Commerce.Models;
+using E_Commerce.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProductController : ControllerBase
+    {
+        IProduct repository;
+        public ProductController(IProduct repository)
+        {
+            this.repository = repository;
+        }
+        [HttpGet]
+        [Route("Get All Products")]
+        [AllowAnonymous]
+        public IActionResult GetAll()
+        {
+            var products = repository.GetAll();
+            return Ok(ConvertFromProductToDTO.ConvertList(products));
+        }
+        [HttpGet]
+        [Route("Get Product By Id")]
+        [AllowAnonymous]
+        public IActionResult Get(int id)
+        {
+            var product = repository.GetById(id);
+            if (product != null)
+            {
+                return Ok(ConvertFromProductToDTO.Convert (product));
+            }
+            return NotFound();
+        }
+        [HttpGet]
+        [Route("Search")]
+        [AllowAnonymous]
+        public IActionResult Search(string keyword)
+        {
+            var search = repository.Search(keyword);
+            if (search.Count!=0)
+            {
+                return Ok(ConvertFromProductToDTO.ConvertList(search));
+            }
+            return new BadRequestObjectResult("No Matching Found");
+        }
+        [HttpDelete]
+        [Route("Delete Product")]
+        public IActionResult Delete(int id)
+        {
+            var product = repository.GetById(id);
+            if (product != null)
+            {
+                repository.Delete(id); 
+                return Ok(ConvertFromProductToDTO.Convert(product));
+            }
+            return new BadRequestObjectResult("No Matching Product");
+        }
+        [HttpPost]
+        [Route("Create New Product")]
+        public IActionResult Create(ProductDTO productDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                var prod = new Product()
+                {
+                    Name = productDTO.Name,
+                    Discount = productDTO.Discount,
+                    Model = productDTO.Model,
+                    CategoryID = productDTO.CategoryID,
+                    Price = productDTO.Price,
+                    Description = productDTO.Description
+                };
+                List<string> productImages = new List<string>();
+                foreach (var item in prod.ProductImages)
+                {
+                    productImages.Add(item.ImageUrl);
+                }
+                productDTO.ProductImages.AddRange(productImages);
+                repository.Create(prod);
+                return Ok(ConvertFromProductToDTO.Convert(prod));
+            }
+            return BadRequest();
+        }
+        [HttpPut]
+        [Route("Update Product")]
+        public IActionResult Update(ProductDTO productDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                var product = repository.GetById(productDTO.Id);
+                if(product != null)
+                {
+                    repository.Update(product);
+                    return Ok(ConvertFromProductToDTO.Convert(product));
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return BadRequest();
+        }
+        [HttpGet]
+        [Route("Get Products By CategoryId")]
+        public IActionResult GetProducts(int id)
+        {
+            var products = repository.GetProductByCatId(id);
+            return Ok(ConvertFromProductToDTO.ConvertList(products));
+        }
+    }
+}
